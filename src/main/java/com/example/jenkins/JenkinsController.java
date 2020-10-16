2f840c3fc56c15619f361a47b5db9fa9dd7c94ad
wuhaiyuan <wu.ak@foxmail.com>
2020-10-16 16:11:42 +0800

    Jenkins持续集成

--- src/main/java/com/example/jenkins/JenkinsController.java
+++ src/main/java/com/example/jenkins/JenkinsController.java
@@ -1,10 +1,12 @@
 package com.example.jenkins;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class JenkinsController {
 
+    @RequestMapping("aaa")
     public String aaa(){
         return  "hello jenkins 测试就Jenkins持续集成！";
     }
